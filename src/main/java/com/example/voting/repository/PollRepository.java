e4d8928442fd3e4f5c8d493ca72ac6ffa3ddcdd4
Dhruv Malik <138770923+Dm-1324@users.noreply.github.com>
2026-09-10 13:36:11 +0530

    Add Poll repository

---
+++ src/main/java/com/example/voting/repository/PollRepository.java
@@ -0,0 +1,6 @@
+package com.example.voting.repository;
+
+import com.example.voting.model.Poll;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PollRepository extends JpaRepository<Poll, Long> {}
